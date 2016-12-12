d1293fb2ab1f0bc4d5ccb909d5488206cea33701
SuperNiac <totodu33290@gmail.com>
2016-12-12 14:35:38 +0100

    supprimer pipe (inutile)

--- pipe.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-//code du pipe a ajouter ici
-
-
--- pipe.h
+++
@@ -1,7 +0,0 @@
-#ifndef PIPE_H_INCLUDED
-#define PIPE_H_INCLUDED
-
-//controle des pipes afin de communiquer entre process
-
-
-#endif // PIPE_H_INCLUDED
