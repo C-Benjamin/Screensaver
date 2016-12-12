7b02155044bf4ad00521e4e785f1d5eb68e28b6b
SuperNiac <totodu33290@gmail.com>
2016-12-12 16:32:18 +0100

    Thomas (#1)
    * ajout .c et .h keyboard
    
    * maj de keyboard et ajout de conio.h
    
    * ajout de conio.h afin de capter les touches
    
    * Ajout du fichier pipe pour communiquer entre programmes
    
    * supprimer pipe (inutile)
    
    * maj keyboard.c
    
    * suppression de keyboard.h

---
+++ keyboard.c
@@ -0,0 +1,38 @@
+#include "keyboard.h"
+#include "conio.h"
+
+#define UP 72
+pid_t sonPid;
+ char ch;
+ char kill = ' ';
+int main()
+{
+    while (1)
+    {
+        ch = getch();
+        printf("%c",ch);
+        if (ch =='\033')
+        {
+            getch();
+            switch(getch())
+            {
+                case 'A':
+                printf(" uup");
+                break;
+                case 'B':
+                printf(" ddown");
+                break;
+                case'C':
+                printf(" rright");
+                break;
+                case'D':
+                printf(" lleft");
+                break;
+            }
+        }
+        if (ch==kill)
+        {
+            printf("kill");
+        }
+    }
+}
