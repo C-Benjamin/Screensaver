756a1bf9d7fae5d2e0e22a35a9761ea389f84581
SuperNiac <totodu33290@gmail.com>
2016-12-12 09:28:00 +0100

    maj de keyboard et ajout de conio.h

--- keyboard.c
+++ keyboard.c
@@ -1,8 +1,17 @@
 #include keyboard.h
+#include <stdio.h>
+#include <stdlib.h>
 
-void getkey();
+
+int getkey();
 {
 
 }
 
 return 0;
+void main()
+{
+    char ch;
+    ch = getchar();
+    printf("test %c"ch);
+}
