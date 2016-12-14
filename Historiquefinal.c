7a1b1fc7b50a9e6a1bd3738de91bfa4978ebac93
Ethace <ced.mevel@gmail.com>
2016-12-14 16:45:47 +0100

    Historique fini

---
+++ Historiquefinal.c
@@ -0,0 +1,255 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct History History;
+struct History
+{
+    int screenlaunched;
+    int  ch;
+    int  aff;
+    int taille;
+    char * tab[100][50];
+    int  nbr;
+    int  st;
+    int  dy;
+    int  in;
+};
+///////////////////////////////////////////////////////////////////////////////////
+void Tri1(History *history)
+{
+    int tabidate = 0;
+
+
+    int j = 0;
+    char* temp [50];
+    printf("\n");
+    while (tabidate == 0)
+    {
+        tabidate = 1;
+        for(j = 0;j < history->taille-1;j++)
+        {
+            if(j < j+1)
+            {
+                strcpy(temp,history->tab[j+1]);
+                strcpy(history->tab[j+1],history->tab[j]);
+                strcpy(history->tab[j],temp);
+
+                 tabidate = 0;
+           }
+        }
+        history->taille--;
+    }
+    int n;
+    printf("\n");
+    for(n = 0;n < history->aff;n++)
+    {
+         printf("%s",history->tab[n]);
+    }
+}
+//////////////////////////////////////////////////////////////////////////////
+void Tri2(History *history)
+{
+    int n;
+     printf("\n");
+ for(n = 0;n < history->ch;n++)
+ {
+    if ((strstr(history->tab[n],"statique")) != NULL)
+    {
+         printf("%s",history->tab[n]);
+    }
+  }
+  for(n = 0;n < history->ch;n++)
+  {
+    if ((strstr(history->tab[n],"dynamique")) != NULL)
+    {
+         printf("%s",history->tab[n]);
+    }
+  }
+  for(n = 0;n < history->ch;n++)
+  {
+    if ((strstr(history->tab[n],"interactif")) != NULL)
+    {
+         printf("%s",history->tab[n]);
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////:
+void affichage(History *history){
+
+//int in = 0;
+//int dy = 0;
+//int st = 0;
+//int nbr = 0;
+//char* tab[100][50];
+char* text;
+char testl[50] = "";
+FILE* file = NULL;
+file = fopen("test.txt","r");
+int c;
+int n = 0;
+
+    if (file != NULL){
+
+      while ((c=fgetc(file)) != EOF)
+      {
+
+        text = fgets(testl,50,file);
+        strcpy(history->tab[n],text);
+        printf("%s",history->tab[n]);
+        n++;
+     }
+     history->aff = n;
+     history->ch = n;
+     history->taille = n;
+     history->nbr = n;
+ ////////////////////////////////////////////////
+   /*int tabidate = 0;
+    history.taille = n;
+
+    int j = 0;
+    char* temp [50];
+    printf("\n");
+    while (tabidate == 0)
+    {
+        tabidate = 1;
+        for(j = 0;j < history.taille-1;j++)
+        {
+            if(j < j+1)
+            {
+                strcpy(temp,tab[j+1]);
+                strcpy(tab[j+1],tab[j]);
+                strcpy(tab[j],temp);
+
+                 tabidate = 0;
+           }
+        }
+        history.taille--;
+    }
+    n = 0;
+    printf("\n");
+    for(n = 0;n < history.aff;n++)
+    {
+         printf("%s",tab[n]);
+    }*/
+///////////////////////////////////////////////////
+ /*printf("\n");
+ for(n = 0;n < history.ch;n++)
+ {
+    if ((strstr(tab[n],"statique")) != NULL)
+    {
+         printf("%s",tab[n]);
+         st++;
+    }
+  }
+
+
+  for(n = 0;n < history.ch;n++)
+  {
+    if ((strstr(tab[n],"dynamique")) != NULL)
+    {
+         printf("%s",tab[n]);
+         dy++;
+    }
+  }
+  for(n = 0;n < history.ch;n++)
+  {
+    if ((strstr(tab[n],"interactif")) != NULL)
+    {
+         printf("%s",tab[n]);
+         in++;
+    }
+  }*/
+//////////////////////////////////////////////////////
+
+
+
+ }
+ /*printf("Stats:\n");
+ printf("Nombre d'historique: %d\n",nbr);
+ printf("Type statique : %d\n",st);
+ printf("Type dynamique : %d\n",dy);
+ printf("Type interactif : %d\n",in);
+fclose(file);*/
+
+}
+
+void Stat(History *history)
+{
+int st=0;
+int dy=0;
+int in=0;
+ printf("Stats:\n");
+ printf("Nombre d'historique: %d\n",history->nbr);
+  int n;
+     printf("\n");
+ for(n = 0;n < history->ch;n++)
+ {
+    if ((strstr(history->tab[n],"statique")) != NULL)
+    {
+
+         st++;
+    }
+  }
+  for(n = 0;n < history->ch;n++)
+  {
+    if ((strstr(history->tab[n],"dynamique")) != NULL)
+    {
+
+         dy++;
+    }
+  }
+  for(n = 0;n < history->ch;n++)
+  {
+    if ((strstr(history->tab[n],"interactif")) != NULL)
+    {
+
+        in++;
+    }
+  }
+
+ printf("Type statique : %d\n",st);
+ printf("Type dynamique : %d\n",dy);
+ printf("Type interactif : %d\n",in);
+}
+
+
+void main()
+{
+    History history;
+    affichage(&history);
+    int choix = 0;
+
+    printf("\n");
+    printf("Tapez 1 pour un tri décroissant\n"
+            "Tapez 2 pour un tri par type \n"
+            "Tapez 3 pour afficher les stats de l'historique\n");
+
+
+    scanf("%d",&choix);
+    system("clear");
+
+    if (choix == 1)
+    {
+        Tri1(&history);
+        printf("\nFait");
+    }
+
+    else if (choix == 2)
+    {
+        Tri2(&history);
+        printf("\nFait");
+    }
+
+    else if (choix == 3)
+    {
+       Stat(&history);
+    }
+
+
+}
+
+
+
