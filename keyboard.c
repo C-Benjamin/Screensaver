6678c27e7a4ad70b86916aacb5edccfd26cb03b2
Ethace <ced.mevel@gmail.com>
2016-12-15 14:40:50 +0100

    Update keyboard.c

--- keyboard.c
+++ keyboard.c
@@ -11,24 +11,24 @@ int main()
     {
 
         ch = getch();
-        if (ch =='\033')
+        if (ch =='\033')// l'information que renvoie les flèche est tout d'abord \033 puis [ et enfin une information uniques pour chaque flèche comme A pour la flèche du haut
         {
             getch();
             switch(getch())
             {
-                case 'A':
+                case 'A': // Cas de la flèche haut
                 ch2[0] = 'a';
-                send(ch2);
+                send(ch2);// Envoie l'information grâce au pipe
                 break;
-                case 'B':
+                case 'B':// Cas de la flèche bas
                 ch2[0] = 'b';
                 send(ch2);
                 break;
-                case'C':
+                case'C':// Cas de la flèche gauche
                 ch2[0] = 'c';
                 send(ch2);
                 break;
-                case'D':
+                case'D':// Cas de la flèche droite
                 ch2[0] = 'd';
                 send(ch2);
                 break;
